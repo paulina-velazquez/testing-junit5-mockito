cc22f4acbc944ff6e51fd05269be2800ab52b2db
paulina_velazquez <mpaulina.velazquez@gmail.com>
2021-06-04 10:26:29 -0400

    Add JUnit extension example

---
+++ src/test/java/com/paulinavelazquez/testingjavajunit5/JUnitExtensionTest.java
@@ -0,0 +1,20 @@
+package com.paulinavelazquez.testingjavajunit5;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Map;
+
+@ExtendWith(MockitoExtension.class)
+public class JUnitExtensionTest {
+
+    @Mock
+    Map<String, Object> mapMock;
+
+    @Test
+    void testMock() {
+        mapMock.put("keyvalue", "foo");
+    }
+}
